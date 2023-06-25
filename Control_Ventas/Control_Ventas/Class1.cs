10c91be707b07708138f3f9acfeef71e6b71f9af
Amena0470 <elbronsom@hotmail.com>
2023-06-25 19:09:55 -0400

    Agregar prodecimiento tabla cargo

---
+++ Control_Ventas/Control_Ventas/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaNegocio;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace Control_Ventas
+{
+    internal class Class1
+    {
+    }
+}
